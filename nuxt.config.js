ed60edfb1de05bebd4d7456436fa32e8d1a9bd2e
Kolaposki <oshodikolapo@gmail.com>
2022-10-28 14:39:37 +0100

    Set up to work locally

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,60 +1,69 @@
 const webpack = require("webpack");
 
 module.exports = {
-    mode: 'spa',
+    mode: "spa",
     head: {
-        title: 'Ntisa Admin Application',
+        title: "Ntisa Admin Application",
         meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' },
-            { name: 'msapplication-TileColor', content: '#da532c' },
-            { name: 'theme-color', content: '#ffffff' }
+            { charset: "utf-8" },
+            { name: "viewport", content: "width=device-width, initial-scale=1" },
+            { hid: "description", name: "description", content: "" },
+            { name: "msapplication-TileColor", content: "#da532c" },
+            { name: "theme-color", content: "#ffffff" },
         ],
         link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@600;700&family=Rubik:wght@700&family=Source+Sans+Pro:wght@400;600;700&display=swap' },
-            { rel: 'stylesheet', href: '/css/uikit.min.css' },
-            { rel: 'stylesheet', href: '/css/croppie.css' },
+            { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
             {
-                rel: 'stylesheet',
-                href: 'https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css'
+                rel: "stylesheet",
+                href: "https://fonts.googleapis.com/css2?family=Montserrat:wght@600;700&family=Rubik:wght@700&family=Source+Sans+Pro:wght@400;600;700&display=swap",
+            },
+            { rel: "stylesheet", href: "/css/uikit.min.css" },
+            { rel: "stylesheet", href: "/css/croppie.css" },
+            {
+                rel: "stylesheet",
+                href: "https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css",
             },
             // { rel: 'stylesheet', href: '/css/style.css' },
-            { rel: 'stylesheet', href: '//cdn.jsdelivr.net/npm/intro.js@2.9.3/introjs.min.css' }
+            {
+                rel: "stylesheet",
+                href: "//cdn.jsdelivr.net/npm/intro.js@2.9.3/introjs.min.css",
+            },
         ],
         script: [
-            { type: 'text/javascript', src: '/js/uikit.min.js' },
-            { type: 'text/javascript', src: '/js/croppie.min.js' },
-            { type: 'text/javascript', src: '/js/uikit-icons.min.js' },
-            { type: 'text/javascript', src: '/js/main.js' },
+            { type: "text/javascript", src: "/js/uikit.min.js" },
+            { type: "text/javascript", src: "/js/croppie.min.js" },
+            { type: "text/javascript", src: "/js/uikit-icons.min.js" },
+            { type: "text/javascript", src: "/js/main.js" },
             // { type: 'text/javascript', src: 'https://unpkg.com/jspdf@latest/dist/jspdf.umd.min.js' },
-            { type: 'text/javascript', src: '/js/intro.js' },
-            { type: 'text/javascript', src: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDLRcwcS-hVc4H8Yejx15PMWWZ934CXgLs&libraries=places' },
-            { type: 'text/javascript', src: 'https://ravesandboxapi.flutterwave.com/flwv3-pug/getpaidx/api/flwpbf-inline.js' },
+            { type: "text/javascript", src: "/js/intro.js" },
+            {
+                type: "text/javascript",
+                src: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDLRcwcS-hVc4H8Yejx15PMWWZ934CXgLs&libraries=places",
+            },
+            {
+                type: "text/javascript",
+                src: "https://ravesandboxapi.flutterwave.com/flwv3-pug/getpaidx/api/flwpbf-inline.js",
+            },
             // { src: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyD9R7EeLdPZusJV_bBsm3EiPj-gwCri8u0&libraries=places'}
             // { type: 'text/javascript', src: 'https://api.ravepay.co/flwv3-pug/getpaidx/api/flwpbf-inline.js'}
-        ]
+        ],
     }, // Headers of the page
     loading: false, // Disable default loading bar
-    dev: process.env.NODE_ENV === 'DEV',
-    css: [
-        '@/static/css/style.css',
-        '~/assets/css/variables.scss'
-    ],
+    dev: process.env.NODE_ENV === "DEV",
+    css: ["@/static/css/style.css", "~/assets/css/variables.scss"],
     build: {
         plugins: [
             new webpack.ProvidePlugin({
-                $: 'jquery',
-                jQuery: 'jquery',
-                'window.jQuery': 'jquery',
-                introJs: ['intro.js'],
+                $: "jquery",
+                jQuery: "jquery",
+                "window.jQuery": "jquery",
+                introJs: ["intro.js"],
                 // jsPDF: 'jspdf'
                 // Croppie: 'croppie'
                 // vuetify: '@nuxtjs/vuetify',
-            })
+            }),
         ],
-        transpile: ['jsPDF'],
+        transpile: ["jsPDF"],
         // extend (config, { isDev, isClient }) {
         // 	if (isDev && isClient) {
         // 		// Run ESLint on save
@@ -75,41 +84,39 @@ module.exports = {
     },
     //Toastr configuration
     toast: {
-        position: 'top-right',
+        position: "top-right",
         duration: 5000,
         fullWidth: false,
         fitToScreen: false,
-        iconPack: 'fontawesome'
+        iconPack: "fontawesome",
     },
-    buildModules: [
-        '@nuxtjs/vuetify'
-    ],
+    buildModules: ["@nuxtjs/vuetify"],
 
     vuetify: {
         customVariables: ["~/assets/css/variables.scss"],
         optionsPath: "./vuetify.options.js",
-        treeShake: true
+        treeShake: true,
     },
     //modules
     modules: [
         // https://go.nuxtjs.dev/axios
-        '@nuxtjs/axios',
-        '@nuxtjs/toast',
-        '@nuxtjs/moment',
+        "@nuxtjs/axios",
+        "@nuxtjs/toast",
+        "@nuxtjs/moment",
         // '@nuxtjs/vuetify',
     ],
     //moment module configuration
     moment: {
-        defaultTimezone: 'Africa/Lagos'
+        defaultTimezone: "Africa/Lagos",
     },
     router: {
-        linkExactActiveClass: 'is-active',
-        middleware: ['auth'],
+        linkExactActiveClass: "is-active",
+        middleware: ["auth"],
     },
     // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
     plugins: [
-        '~/plugins/global.js',
-        { src: '~/plugins/library.js', mode: 'client' }
+        "~/plugins/global.js",
+        { src: "~/plugins/library.js", mode: "client" },
     ],
     /**
      * Axios module
@@ -118,22 +125,23 @@ module.exports = {
     // http://162.0.233.96/API dev server
     //
     axios: {
-        baseURL: process.env.BASE_URL ||
-            'https://wajeloyalty.website/',
+        baseURL: "http://127.0.0.1:9000/",
+        //  process.env.BASE_URL || "https://wajeloyalty.website/",
         //'https://wajeloyalty.website/', // 'http://127.0.0.1:8000/', //'http://162.0.233.96/', // ||
-        proxy: false
+        proxy: false,
     },
     proxy: {
-        '/api/': 'https://wajeloyalty.website//'
-            //'https://wajeloyalty.website/' //http://162.0.233.96/' 
+        "/api/": "https://wajeloyalty.website//",
+        //'https://wajeloyalty.website/' //http://162.0.233.96/'
     },
     //environment variables
     env: {
-        BASE_URL: 'https://wajeloyalty.website//',
+        BASE_URL: "http://127.0.0.1:9000/",
+        // "https://wajeloyalty.website//",
         //'https://wajeloyalty.website/', // 'http://127.0.0.1:8000/',//'http://162.0.233.96/' //
     },
 
     // srcDir: '../'
 
     // rootDir: '../../NTISA-Merhcant-Frontend'
-}
+};
