babe17d4662be5fc4ad138281991c7de6bdb0372
Kolapo Oshodi <oshodikolapo@gmail.com>
2022-10-31 08:34:16 +0000

    Update ngrok url

--- nuxt.config.js
+++ nuxt.config.js
@@ -127,7 +127,7 @@ module.exports = {
     // http://162.0.233.96/API dev server
     //
     axios: {
-        baseURL: "https://3235-102-219-153-218.eu.ngrok.io/",
+        baseURL: "https://16ee-62-173-44-154.eu.ngrok.io/",
         //  process.env.BASE_URL || "https://wajeloyalty.website/",
         //'https://wajeloyalty.website/', // 'http://127.0.0.1:8000/', //'http://162.0.233.96/', // ||
         proxy: false,
@@ -138,7 +138,7 @@ module.exports = {
     },
     //environment variables
      env: {
-        BASE_URL: "https://3235-102-219-153-218.eu.ngrok.io/",
+        BASE_URL: "https://16ee-62-173-44-154.eu.ngrok.io",
         // "https://wajeloyalty.website//",
         //'https://wajeloyalty.website/', // 'http://127.0.0.1:8000/',//'http://162.0.233.96/' //
     },
